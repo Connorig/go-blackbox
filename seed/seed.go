cacbc7a2f6cea2d0be682ea174b6c58b7655ab3c
homelander <2410039231@qq.com>
2023-05-07 21:28:52 +0800

    comm

--- seed/seed.go
+++ seed/seed.go
@@ -2,6 +2,7 @@ package seed
 
 import (
 	"context"
+	"errors"
 	"github.com/Domingor/go-blackbox/appioc"
 	"github.com/Domingor/go-blackbox/server/zaplog"
 	"go.uber.org/zap"
@@ -12,6 +13,7 @@ import (
 //	Init(appioc context.Context) (err error)
 //}
 
+// SeedFunc 自定义种子函数
 type SeedFunc func(etc context.Context) (err error)
 
 // Seed exec seed funcs
@@ -19,14 +21,16 @@ func Seed(SeedFunctions ...SeedFunc) error {
 	zaplog.ZAPLOG.Debug("Seed funcs are running now.")
 
 	if len(SeedFunctions) == 0 {
-		return nil
+		return errors.New("there is no seed func needed to run")
 	}
 	for _, v := range SeedFunctions {
+		// 批量执行种子函数，传入上下文对象
 		err := v(appioc.GetContext().Ctx)
 		if err != nil {
 			zaplog.ZAPLOG.Error("Seed func running fail.", zap.Any("err", err))
 			return err
 		}
 	}
+	zaplog.ZAPLOG.Info("all seed func are run now")
 	return nil
 }
