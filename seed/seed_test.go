63ac8a59da3c64b8caa1f1d9785fed6f03a9855c
connor-g <2417039231@qq.com>
2024-06-07 11:10:30 +0800

    updated

--- seed/seed_test.go
+++ seed/seed_test.go
@@ -9,11 +9,12 @@ import (
 func TestSeed(t *testing.T) {
 	t.Run("test seed data", func(t *testing.T) {
 		err := Seed(func(etc context.Context) (err error) {
-			fmt.Println("hello! ")
+			fmt.Println("hello!")
 			return nil
 		})
 		if err != nil {
 			t.Error(err)
+
 		}
 	})
 }
