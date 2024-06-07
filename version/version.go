547876699e54618e711e145f18c15e8827f6d84e
connor-g <2417039231@qq.com>
2024-06-07 11:45:12 +0800

    updated

---
+++ version/version.go
@@ -0,0 +1,31 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+/**
+* @Author: Connor
+* @Date:   24.6.7 11:44
+* @Description:
+ */
+
+var (
+	AppName  = ""
+	Version  = ""
+	Commit   = ""
+	Build    = ""
+	Compiler = ""
+)
+
+func Print() {
+	fmt.Println("***********************************")
+	fmt.Printf("Name     :%s\n", AppName)
+	fmt.Printf("Version  :%s\n", Version)
+	fmt.Printf("Commit   :%s\n", Commit)
+	fmt.Printf("Build    :%s\n", Build)
+	fmt.Printf("Compiler :%s\n", Compiler)
+	fmt.Printf("Runtime  :%s/%s\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Println("***********************************")
+}
